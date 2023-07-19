1aa9925e364d399ca35104e50c51cb3f84435c5c
Evan Woods <e.woods.business@icloud.com>
2023-07-19 10:20:08 -0400

    commit

---
+++ docker_build.sh
@@ -0,0 +1,1 @@
+DOCKER_BUILDKIT=1 docker build -t ml-python-cookbook-runner .
---
+++ run.sh
@@ -0,0 +1,1 @@
+docker run -it -v /Users/evanwoods/Github/ml-python-cookbook-runner:/workspace -p 8888:8888 ml-python-cookbook-runner
